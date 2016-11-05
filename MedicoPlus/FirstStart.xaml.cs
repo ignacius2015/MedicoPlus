f0ab78a152f8fa86bc8b7e7cf31695a4c35ecce1
ignacius <petrovskij@hotmail.com>
2016-11-05 09:31:40 +0200

    1

---
+++ MedicoPlus/FirstStart.xaml.cs
@@ -0,0 +1,104 @@
+﻿using MedicoPlus.FolderBrowser;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace MedicoPlus
+{
+    
+    struct sett
+    {
+        public string docs;
+        public string forms;
+        public string templates;
+        public string database;
+        public string basename;
+
+        public sett(string d, string f, string t, string da, string ba)
+        {
+            docs = d;
+            forms = f;
+            templates = t;
+            database = da;
+            basename = ba;
+        }
+    }
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class FirstStart : Window
+    {
+        string[] databases = { "MySQL", "Microsoft SQL Server", "PostgreSQL", "ORACLE Database", "MongoDB", "SyBase", "SQLite" };
+        public static string filename = System.IO.Path.Combine(Environment.CurrentDirectory, "settings.dat");
+        public FirstStart()
+        {
+            this.InitializeComponent();
+            databasesList.ItemsSource = databases;
+            DocsFolder.Text = System.IO.Path.Combine(Environment.CurrentDirectory, "UserDocs");
+            FormFolder.Text = System.IO.Path.Combine(Environment.CurrentDirectory, "UserForms");
+            TemplatePath.Text= System.IO.Path.Combine(Environment.CurrentDirectory, "Templates");
+            // Insert code required on object creation below this point.
+        }
+
+
+        private void buttonDocs_Click(object sender, RoutedEventArgs e)
+        {
+            DialogSelectFolder SelectFolder = new DialogSelectFolder();
+            SelectFolder.ShowDialog();
+            DocsFolder.Text = csPathToFolder.PathOfSelectedFolder;
+            SelectFolder.Close();
+        }
+
+        private void buttonFolder_Click(object sender, RoutedEventArgs e)
+        {
+            DialogSelectFolder SelectFolder = new DialogSelectFolder();
+            SelectFolder.ShowDialog();
+            FormFolder.Text=csPathToFolder.PathOfSelectedFolder;
+            SelectFolder.Close();
+        }
+
+        private void databasesList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // TODO: Add event handler implementation here.
+        }
+
+        private void TemplateChoice_Click(object sender, RoutedEventArgs e)
+        {
+            DialogSelectFolder SelectFolder = new DialogSelectFolder();
+            SelectFolder.ShowDialog();
+            TemplatePath.Text= csPathToFolder.PathOfSelectedFolder;
+        }
+
+        
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            var obj = new sett(DocsFolder.Text, FormFolder.Text, TemplatePath.Text,databasesList.SelectedItem.ToString(), databaseName.Text);
+            
+            FileStream fout = File.OpenWrite(filename);
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(fout, obj);
+            fout.Close();
+
+            //FileStream fin = File.OpenRead(path);
+
+            //BinaryFormatter bf = new BinaryFormatter();
+
+            //newBody = (body)bf.Deserialize(fs);
+
+            //fin.Close();
+        }
+    }
+}
---
+++ MedicoPlus/FolderBrowser/DialogSelectFolder.xaml.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MedicoPlus.FolderBrowser
+{
+    /// <summary>
+    /// Interaction logic for DialogSelectFolder.xaml
+    /// </summary>
+    public partial class DialogSelectFolder : Window
+    {
+        public DialogSelectFolder()
+        {
+            InitializeComponent();
+
+            // При вызове показывать Заполнить Листбокс, списком локальных дисков
+            GetAddDrivesToListBox();
+        }
+
+        // Отмена
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        // Я Выбрал
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            if (csPathToFolder.PathOfSelectedFolder != null)
+            {
+                csPathToFolder.PathOfSelectedFolder = textBlock1.Text;
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Оберіть каталог");
+            }
+
+        }
+
+        // На уровыень выше
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            string currentPath = csPathToFolder.PathOfSelectedFolder;
+            int curPathLength = currentPath.Length;
+            // Определяем количество слэшей в пути
+            string pattern = "\\";
+            int slashCount = new Regex(Regex.Escape(pattern)).Matches(currentPath).Count;
+            // MessageBox.Show(amount.ToString()); C:/Work/folder - 2
+            if (currentPath == null)
+            {
+                // GetAddDrivesToListBox(); // Если диск не выбран - показываем диски
+            }
+
+            else
+            {
+                // C:/ Если слэш один и путь состоит из названия диска - Показываем диски
+                if ((slashCount <= 1) && (curPathLength <= 3))
+                {
+                    GetAddDrivesToListBox();
+                }
+                // C:/Work Если слэш один, но это верхняя папка на диске
+                else if ((slashCount <= 1) && (curPathLength > 3))
+                {
+                    // находим слэш и удаляем знаки до конца строки C:/Work -> C:/
+                    int slashZnak = currentPath.IndexOf('\\') + 1;
+                    currentPath = currentPath.Remove(slashZnak);
+                    csPathToFolder.PathOfSelectedFolder = null; // очищаем значение
+                    csPathToFolder.PathOfSelectedFolder = currentPath;
+                    textBlock1.Text = csPathToFolder.PathOfSelectedFolder;
+                    GetAddItemsToListBox();
+                }
+                //C:/Work/folder если Слэшей больше одного
+                else
+                {
+                    // показываем путь отняв последний слэш C:/Work/folder -> C:/Work/folder
+                    int position = currentPath.LastIndexOf('\\'); // где в последний раз находили слэш - +1 
+                    csPathToFolder.PathOfSelectedFolder = null; // очищаем значение
+                    csPathToFolder.PathOfSelectedFolder = currentPath.Remove(position);
+                    textBlock1.Text = csPathToFolder.PathOfSelectedFolder;
+                    GetAddItemsToListBox();
+                }
+
+            }
+
+        }
+
+        // Два раза кликаем для того что бы войти в каталог
+        private void mouseDoubleClickOnListBox(object sender, MouseButtonEventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            // MessageBox.Show(listBox1.SelectedItem.ToString());
+            {
+
+                csPathToFolder.PathOfSelectedFolder = listBox1.SelectedItem.ToString();
+                // Заполняем листбокс найденными каталогами
+                GetAddItemsToListBox();
+            }
+        }
+
+        // Заносим текущее выделение в Путь
+        private void currentSelect(object sender, SelectionChangedEventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            // MessageBox.Show(listBox1.SelectedItem.ToString());
+            {
+                textBlock1.Text = listBox1.SelectedItem.ToString();
+                csPathToFolder.PathOfSelectedFolder = listBox1.SelectedItem.ToString();
+
+            }
+        }
+        #region Методы
+        // Выводим папки
+        private void GetAddItemsToListBox()
+        {
+            textBlock1.Text = csPathToFolder.PathOfSelectedFolder;
+            string[] folderArr = Directory.GetDirectories(csPathToFolder.PathOfSelectedFolder);
+            listBox1.Items.Clear();
+            foreach (string folders in folderArr)
+            {
+                listBox1.Items.Add(folders);
+            }
+        }
+
+        // Заполняем Листбокс локальными дисками
+        private void GetAddDrivesToListBox()
+        {
+            string[] myDrives = Environment.GetLogicalDrives();
+            csPathToFolder.PathOfSelectedFolder = null;
+            textBlock1.Text = csPathToFolder.PathOfSelectedFolder;
+            listBox1.Items.Clear();
+            foreach (string myDrive in myDrives)
+            {
+                listBox1.Items.Add(myDrive);
+            }
+        }
+        #endregion
+    }
+    // Путь к папке, общий для всех классов
+    public static class csPathToFolder
+    {
+        public static string PathOfSelectedFolder { get; set; }
+        public static string SelectedDrive { get; set; }
+
+    }
+}
